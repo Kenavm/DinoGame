4bc4608b86424a7dc9c6d68d3d39ae76c98dc95e
Manuel <manuel.vanek@gmail.com>
2022-12-03 14:01:34 +0100

    dinogame v1

--- index.js
+++ index.js
@@ -3,7 +3,34 @@ const WIDTH = 800;
 const HEIGHT = 256;
 
 const DINO_EL = document.querySelector("#dino");
+const cactus = document.getElementById("cactus");
 
-function main() {}
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
 
-main();
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 500);
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+    jump();
+  });
+
+  let isAlive = setInterval(function () {
+    // get current dino Y position
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  
+    // get current cactus X position
+    let cactusLeft = parseInt(
+      window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+  
+    // detect collision
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+      // collision
+      alert("Game Over!");
+    }
+  }, 10);
