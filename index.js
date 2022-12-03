6fbb19418b3b947fe72852123cb498ca9ab1a953
AdrianMonemi <adrianmonemi6@gmail.com>
2022-12-03 16:34:42 +0100

    DinoGame Ver.3

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 // The size of our game area
 const WIDTH = 800;
 const HEIGHT = 256;
-
 const DINO_EL = document.querySelector("#dino");
-const CACTUS = document.getElementById("cactus");
+const cactus = document.querySelector("#cactus");
+const scoreDiv = document.querySelector("#score");
+
+let cactusLeft = parseInt(
+window.getComputedStyle(cactus).getPropertyValue("left")
+);
 
 function jump() {
-  if (DINO_EL.classList != "jump") {
-    DINO_EL.classList.add("jump");
+if (DINO_EL.classList != "jump") {
+DINO_EL.classList.add("jump");
 
-    setTimeout(function () {
-      DINO_EL.classList.remove("jump");
-    }, 500);
-  }
+setTimeout(function () {
+DINO_EL.classList.remove("jump");
+}, 500);
 }
-
+}
+let score = 0;
 document.addEventListener("keydown", function (event) {
-    jump();
-  });
+jump();
+});
 
-  let isAlive = setInterval(function () {
-    // get current dino Y position
-    let dinoTop = parseInt(window.getComputedStyle(DINO_EL).getPropertyValue("top"));
-  
-    // get current cactus X position
-    let cactusLeft = parseInt(
-      window.getComputedStyle(CACTUS).getPropertyValue("left")
-    );
-  
-    // detect collision
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-      // collision
-      alert("Game Over!");
-    }
-  }, 10);
+let isAlive = setInterval(function () {
+let dinoTop = parseInt(window.getComputedStyle(DINO_EL).getPropertyValue("top"));
+let dinoBottom = parseInt(window.getComputedStyle(DINO_EL).getPropertyValue("bottom"));
+
+let cactusLeft = parseInt(
+window.getComputedStyle(cactus).getPropertyValue("left")
+);
+if (cactusLeft < 110 && cactusLeft > 60 && dinoBottom <= 49) {
+score = 0;
+scoreDiv.textContent = score;
+
+alert("Game Over!");
+}
+let cactusPassed = false;
+console.log('A' + cactusPassed)
+
+if(cactusLeft < 40 && !cactusPassed){
+  cactusPassed = true;
+console.log('B' + cactusPassed)
+score++;
+scoreDiv.textContent = score;
+}
+}, 10);
