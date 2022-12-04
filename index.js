80190a3b8c2e3b08f9b6a446cbc74f8b2f1259bb
AdrianMonemi <adrianmonemi6@gmail.com>
2022-12-04 16:58:09 +0100

    running floor-jump-score

--- index.js
+++ index.js
@@ -6,43 +6,52 @@ const cactus = document.querySelector("#cactus");
 const scoreDiv = document.querySelector("#score");
 
 let cactusLeft = parseInt(
-window.getComputedStyle(cactus).getPropertyValue("left")
+  window.getComputedStyle(cactus).getPropertyValue("left")
 );
 
 function jump() {
-if (DINO_EL.classList != "jump") {
-DINO_EL.classList.add("jump");
+  if (DINO_EL.classList != "jump") {
+    DINO_EL.classList.add("jump");
 
-setTimeout(function () {
-DINO_EL.classList.remove("jump");
-}, 500);
-}
+    setTimeout(function () {
+      DINO_EL.classList.remove("jump");
+    }, 500);
+  }
 }
 let score = 0;
 document.addEventListener("keydown", function (event) {
-jump();
+  jump();
 });
 
 let isAlive = setInterval(function () {
-let dinoTop = parseInt(window.getComputedStyle(DINO_EL).getPropertyValue("top"));
-let dinoBottom = parseInt(window.getComputedStyle(DINO_EL).getPropertyValue("bottom"));
+  let dinoTop = parseInt(
+    window.getComputedStyle(DINO_EL).getPropertyValue("top")
+  );
+  let dinoBottom = parseInt(
+    window.getComputedStyle(DINO_EL).getPropertyValue("bottom")
+  );
 
-let cactusLeft = parseInt(
-window.getComputedStyle(cactus).getPropertyValue("left")
-);
-if (cactusLeft < 110 && cactusLeft > 60 && dinoBottom <= 49) {
-score = 0;
-scoreDiv.textContent = score;
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
 
-alert("Game Over!");
-}
-let cactusPassed = false;
-console.log('A' + cactusPassed)
-
-if(cactusLeft < 40 && !cactusPassed){
-  cactusPassed = true;
-console.log('B' + cactusPassed)
-score++;
-scoreDiv.textContent = score;
-}
+  if (cactusLeft < 110 && cactusLeft > 60 && dinoTop >= 185) {
+    score = 0;
+    scoreDiv.textContent = score;
+    alert("Game Over!");
+  }
+  scoreCheck(cactusLeft);
 }, 10);
+
+function scoreCheck(a) {
+  let cactusPassed = false;
+  if (a < 40 && !cactusPassed) {
+    score++;
+    scoreDiv.textContent = Math.floor(score / 22);
+  }
+}
+while (true) {
+  groundMove();
+  // groundMove2();
+  // groundMove3();
+}
